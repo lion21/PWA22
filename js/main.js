e447a729926459a0e08e2a745473cedf669a900f
lion21 <lion21as@gmail.com>
2014-05-13 18:31:37 -0400

    changed repo dir

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/*  
+Susan Todd
+Project Management App
+5-5-2014
+PWA-2
+*/
+
+(function($){
+	console.log("ready");//ready
+	$("#signInButton").click(function(){
+
+		var user = $("#user").val();
+		var pass = $("#pass").val();
+		consloe.log("pass working?");
+
+	});
+
+	
+	
+	
+	/*
+	===============================================
+	========================= APPLICATION FUNCTIONS	
+	*/
+	
+	
+	var checkLoginState = function(){
+		$.ajax({
+			url: 'xhr/check_login.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+				// if user, loadApp()
+				// if error, loadLanding()
+			}
+		});
+	};
+	
+	
+
+	// 	============================================
+	//	SETUP FOR INIT
+		
+	var init = function(){
+	
+		checkLoginState();
+	};
+	
+	
+	init();
+	
+		
+	/*
+	===============================================
+	======================================== EVENTS	
+	*/
+	
+	
+	/*	
+	==================================== END EVENTS 
+	===============================================
+	*/
+		
+		
+
+	
+})(jQuery); // end private scope
+
+
+
+
